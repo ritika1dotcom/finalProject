5c6e8158f699fae866d9cd82afcfddbd1064937c
ritika1dotcom <r51316099@gmail.com>
2024-02-24 11:07:16 +0545

    feedback form

--- spotify/admin.py
+++ spotify/admin.py
@@ -2,4 +2,8 @@
 from django.contrib import admin
 from .models import Feedback
 
-admin.site.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'algorithm', 'user_age_group', 'user_favorite_genre', 'feedback_date')
+    search_fields = ('user__username', 'algorithm', 'user__user_preferences__age_group', 'user__user_preferences__favorite_music_genre')
+
+admin.site.register(Feedback, FeedbackAdmin)
